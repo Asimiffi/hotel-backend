50efa5596516c00cd31712c4347350571159ec69
Asim Iftikhar <51819908+Asimiffi@users.noreply.github.com>
2022-10-30 16:19:27 +0500

    Update server.js

--- server.js
+++ server.js
@@ -19,5 +19,5 @@ app.use('/api/rooms',roomsRoute)
 app.use('/api/users',userRoute)
 app.use('/api/bookings',bookingRoute)
 
-const port = 5000 || process.env.Port;
+const port = process.env.Port || 5000;
 app.listen(port, ()=>console.log('nodemon started node'))
