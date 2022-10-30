3237518d387f14f35c0314015b030d4208bfaa58
Asim Iftikhar <asim.nustian@hotmail.com>
2022-10-30 15:16:50 +0500

    first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const roomsRoute = require('./routes/roomRoute')
+const userRoute = require('./routes/userRoute')
+const bookingRoute = require('./routes/bookingsRoute')
+require('dotenv').config()
+
+const cors = require('cors')
+
+app.use(express.json());
+app.use(cors());
+
+const mongoURL = 'mongodb+srv://asim:asim@cluster0.m9eim.mongodb.net/mern-rooms';
+
+mongoose.connect(mongoURL, {useUnifiedTopology:true, useNewURLParser: true})
+
+app.use('/api/rooms',roomsRoute)
+app.use('/api/users',userRoute)
+app.use('/api/bookings',bookingRoute)
+
+const port = 5000 || process.env.port;
+app.listen(port, ()=>console.log('nodemon started node'))
